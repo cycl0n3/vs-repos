2f494ce78923a67c82081d73e06ac4121fef764c
Milind <7milind7@gmail.com>
2023-05-08 18:12:30 +0530

    more something.

---
+++ Project2/Project2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// import opencv header files
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+int main()
+{
+    cv::Mat shops;
+    shops = cv::imread("shops.jpg", cv::IMREAD_COLOR);
+
+    if (shops.empty())
+    {
+        std::cout << "Could not read the image: " << "shops.jpg" << std::endl;
+        return 1;
+    }
+
+    cv::resize(shops, shops, cv::Size(), 1.0, 0.75);
+
+    cv::namedWindow("Shops", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Shops", shops);
+
+    cv::Mat shops_gray;
+    cv::cvtColor(shops, shops_gray, cv::COLOR_BGR2GRAY);
+    cv::rotate(shops_gray, shops_gray, cv::ROTATE_90_CLOCKWISE);
+
+    cv::namedWindow("Shops Gray", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Shops Gray", shops_gray);
+
+    cv::waitKey(0);
+    return 0;
+}
