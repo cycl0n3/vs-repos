8e11b86a4c201b6ce65ef07d04a3865a0b0cff7f
Milind <7milind7@gmail.com>
2023-05-27 23:59:26 +0530

    some useless projects

---
+++ AvaloniaApplication1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace AvaloniaApplication1.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public void button_Click(object sender, RoutedEventArgs e)
+        {
+            // Change button text when button is clicked.
+            var button = (Button)sender;
+            button.Content = "Hello, Avalonia!";
+        }
+    }
+}
